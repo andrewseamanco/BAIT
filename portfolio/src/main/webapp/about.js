eb87ca88b472643856f04da1e122fab7ca9a99cd
Andrew Seaman <andrew.seamanco@gmail.com>
2020-05-28 14:31:57 -0600

    Add a function which adds new photos and alerts when wrong or correct

--- portfolio/src/main/webapp/about.js
+++ portfolio/src/main/webapp/about.js
@@ -1,6 +1,7 @@
-console.log(truths);
+let answer = 0;
 
 function loadInTruth() {
+
     let firstTruth = Math.floor(Math.random() * truths.length);
     let secondTruth = Math.floor(Math.random() * truths.length);
     while (firstTruth == secondTruth && truths.length != 1) {
@@ -12,6 +13,10 @@ function loadInTruth() {
     while (firstPlace == secondPlace) {
         secondPlace = Math.floor(Math.random() * 3);
     }
+
+    let truthLocations = new Array(2);
+    truthLocations[0] = firstPlace;
+    truthLocations[1] = secondPlace;
 
     if (firstPlace==0) {
         document.getElementById("first_txt").innerHTML = truths[firstTruth][0];
@@ -34,6 +39,55 @@ function loadInTruth() {
         document.getElementById("third_txt").innerHTML = truths[secondTruth][0];
         document.getElementById("third_img").src = truths[secondTruth][1];
     }
+
+    return truthLocations;
 }
 
-loadInTruth();
+function loadInLie(truthLocations) {
+    //determine the location without a truth value
+    let lieLocation = (3 - (truthLocations[0] + truthLocations[1]));
+
+    let lie = Math.floor(Math.random() * lies.length);
+    
+    if (lieLocation==0) {
+        document.getElementById('first_txt').innerHTML = lies[lie][0];
+        document.getElementById('first_img').src = lies[lie][1];
+    } else if (lieLocation==1) {
+        document.getElementById('second_txt').innerHTML = lies[lie][0];
+        document.getElementById('second_img').src = lies[lie][1];
+    } else if (lieLocation==2) {
+        document.getElementById('third_txt').innerHTML = lies[lie][0];
+        document.getElementById('third_img').src = lies[lie][1];
+    }
+    return lieLocation;
+}
+
+function loadInNewQuestion() {
+    let truthLocations = loadInTruth();
+    answer = loadInLie(truthLocations);
+}
+
+function next()  {
+    let q_num = document.getElementById("question_num");
+    if (q_num.innerHTML=="Press Start to Begin!") {
+        document.getElementById("question_num").innerHTML = 1;
+        loadInNewQuestion();
+        document.getElementById("two_truths").innerHTML = "Next";
+    } else {
+        let selectedAnswer = -1;
+        let chosenRadios = document.querySelectorAll("input[name='chosen_answer']");
+        for (chosenRadio of chosenRadios) {
+            if(chosenRadio.checked) {
+                selectedAnswer = chosenRadio.value;
+            }
+        }
+        if (selectedAnswer!=-1) {
+            if (answer==selectedAnswer) {
+                alert("This is the lie!");
+            } else {
+                alert("This is the truth!");
+            }
+            loadInNewQuestion();
+        }
+    }
+}
