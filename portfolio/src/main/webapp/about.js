876e2502f467de784e34a5df7448e80fb1994b37
Andrew Seaman <andrew.seamanco@gmail.com>
2020-05-29 11:46:29 -0600

    Renamed variables and created LoadInFact to reduce duplicated code

--- portfolio/src/main/webapp/about.js
+++ portfolio/src/main/webapp/about.js
@@ -1,65 +1,54 @@
 let answer = 0;
 
-function loadInTruth() {
 
-    let firstTruth = Math.floor(Math.random() * truths.length);
-    let secondTruth = Math.floor(Math.random() * truths.length);
-    while (firstTruth == secondTruth) {
-        secondTruth = Math.floor(Math.random() * truths.length);
+function loadInFact(index, fact_index, isTruth) {
+    let fact = isTruth ? truths[fact_index] : lies[fact_index];
+
+    switch(index) {
+        case 0:
+            document.getElementById("first_txt").innerHTML = fact[0];
+            document.getElementById("first_img").src = fact[1];
+            break;
+        case 1:
+            document.getElementById("second_txt").innerHTML = fact[0];
+            document.getElementById("second_img").src = fact[1];
+            break;
+        case 2:
+            document.getElementById("third_txt").innerHTML = fact[0];
+            document.getElementById("third_img").src = fact[1];
+            break;
     }
-
-    let firstPlace = Math.floor(Math.random() * 3);
-    let secondPlace = Math.floor(Math.random() * 3);
-    while (firstPlace == secondPlace) {
-        secondPlace = Math.floor(Math.random() * 3);
-    }
-
-    let truthLocations = new Array(2);
-    truthLocations[0] = firstPlace;
-    truthLocations[1] = secondPlace;
-
-    if (firstPlace==0) {
-        document.getElementById("first_txt").innerHTML = truths[firstTruth][0];
-        document.getElementById("first_img").src = truths[firstTruth][1];
-    } else if (firstPlace==1) {
-        document.getElementById("second_txt").innerHTML = truths[firstTruth][0];
-        document.getElementById("second_img").src = truths[firstTruth][1];
-    } else if (firstPlace==2) {
-        document.getElementById("third_txt").innerHTML = truths[firstTruth][0];
-        document.getElementById("third_img").src = truths[firstTruth][1];
-    }
-
-    if (secondPlace == 0) {
-        document.getElementById("first_txt").innerHTML = truths[secondTruth][0];
-        document.getElementById("first_img").src = truths[secondTruth][1];
-    } else if (secondPlace==1) {
-        document.getElementById("second_txt").innerHTML = truths[secondTruth][0];
-        document.getElementById("second_img").src = truths[secondTruth][1];
-    } else if (secondPlace==2) {
-        document.getElementById("third_txt").innerHTML = truths[secondTruth][0];
-        document.getElementById("third_img").src = truths[secondTruth][1];
-    }
-
-    return truthLocations;
 }
 
-function loadInLie(truthLocations) {
-    //determine the location without a truth value
-    let lieLocation = (3 - (truthLocations[0] + truthLocations[1]));
+function loadInTruth() {
 
-    let lie = Math.floor(Math.random() * lies.length);
-    
-    if (lieLocation==0) {
-        document.getElementById('first_txt').innerHTML = lies[lie][0];
-        document.getElementById('first_img').src = lies[lie][1];
-    } else if (lieLocation==1) {
-        document.getElementById('second_txt').innerHTML = lies[lie][0];
-        document.getElementById('second_img').src = lies[lie][1];
-    } else if (lieLocation==2) {
-        document.getElementById('third_txt').innerHTML = lies[lie][0];
-        document.getElementById('third_img').src = lies[lie][1];
+    let first_truth_value = Math.floor(Math.random() * truths.length);
+    let second_truth_value = Math.floor(Math.random() * truths.length);
+    while (first_truth_value == second_truth_value) {
+        second_truth_value = Math.floor(Math.random() * truths.length);
     }
-    return lieLocation;
+
+    let first_truth_index = Math.floor(Math.random() * 3);
+    let second_truth_index = Math.floor(Math.random() * 3);
+    while (first_truth_index == second_truth_index) {
+        second_truth_index = Math.floor(Math.random() * 3);
+    }
+
+    loadInFact(first_truth_index, first_truth_value, true);
+    loadInFact(second_truth_index, second_truth_value, true);
+
+    return [first_truth_index, second_truth_index];
+}
+
+
+function loadInLie(truth_indeces) {
+    //determine the location without a truth value
+    let lie_index = (3 - (truth_indeces[0] + truth_indeces[1]));
+    
+    let lie_value = Math.floor(Math.random() * lies.length);
+    loadInFact(lie_index, lie_value, false);
+
+    return lie_index;
 }
 
 function loadInNewQuestion() {
