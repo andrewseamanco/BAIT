8aa9483b00efd63c0e2b0d73807b2b6d18218ccc
Andrew Seaman <andrew.seamanco@gmail.com>
2020-06-02 18:08:33 +0000

    Remove comments

--- portfolio/src/main/webapp/community.js
+++ portfolio/src/main/webapp/community.js
@@ -1,6 +1,5 @@
 function renderComments() {
   fetch('/comments').then(response => response.json()).then((conversation) => {
-    console.log(conversation);
     const commentSection = document.getElementById("submitted-comments");
     conversation.commentList.forEach((comment) => {
         const usernameElement = document.createElement("p");
