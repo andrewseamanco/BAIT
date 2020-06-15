3a81dd7c5456906d30bd668a124a914e5bafeeca
Andrew Seaman <andrew.seamanco@gmail.com>
2020-06-15 21:35:14 +0000

    Add a method to find meeting times

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,57 @@
 
 package com.google.sps;
 
+import java.util.Comparator;
+import java.util.Collections;
 import java.util.Collection;
+import java.util.stream.Collectors; 
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+public final class FindMeetingQuery implements Comparator<Event> {
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    List<Event> eventList = events.stream().collect(Collectors.toList());
+    Collections.sort(eventList, new FindMeetingQuery());
+    int start = TimeRange.START_OF_DAY;
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    for (int i = 0; i < eventList.size(); i++) {
+        if (hasMandatoryAttendees(eventList.get(i).getAttendees(), request.getAttendees())) {
+            TimeRange eventRange = eventList.get(i).getWhen();
+
+            if (start < eventRange.start()) {
+                TimeRange range = TimeRange.fromStartEnd(start, eventRange.start(), false);
+                if (range.duration() >= request.getDuration()) {
+                    availableTimes.add(range);
+                }
+            }
+            if (start < eventRange.end()) {
+                start = eventRange.end();
+            }
+        }
+    }
+    TimeRange range = TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY,true);
+    if (range.duration() >= request.getDuration()) {
+        availableTimes.add(range);
+    }
+    return availableTimes;
   }
+
+    public int compare(Event a, Event b) { 
+        //sort by start
+        return a.getWhen().start() - b.getWhen().start(); 
+    }
+
+    public static boolean hasMandatoryAttendees(Set eventAttendees, Collection<String> mandatoryAttendees) {
+        for (String mandatoryAttendee : mandatoryAttendees) {
+            if (eventAttendees.contains(mandatoryAttendee)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
