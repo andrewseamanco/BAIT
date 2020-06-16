cef5104272c3340b07de1f2e856e2ffa0381723b
Andrew Seaman <andrew.seamanco@gmail.com>
2020-06-16 00:27:18 +0000

    Create methods for blocks so that optional is possible

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -29,11 +29,54 @@ public final class FindMeetingQuery implements Comparator<Event> {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     List<Event> eventList = events.stream().collect(Collectors.toList());
     Collections.sort(eventList, new FindMeetingQuery());
-    int start = TimeRange.START_OF_DAY;
-    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    int mandatoryStart = TimeRange.START_OF_DAY;
+    int optionalStart = TimeRange.START_OF_DAY;
+    Collection<TimeRange> mandatoryAvailableTimes = new ArrayList<TimeRange>();
+    Collection<TimeRange> optionalAvailableTimes = new ArrayList<TimeRange>();
     for (int i = 0; i < eventList.size(); i++) {
-        if (hasMandatoryAttendees(eventList.get(i).getAttendees(), request.getAttendees())) {
-            TimeRange eventRange = eventList.get(i).getWhen();
+        checkIfEventConflicts(mandatoryAvailableTimes, eventList.get(i), request, true, mandatoryStart);
+        checkIfEventConflicts(optionalAvailableTimes, eventList.get(i), request, false, optionalStart);
+        TimeRange eventRange = eventList.get(i).getWhen();
+        if (mandatoryStart < eventRange.end() && hasAttendees(eventList.get(i).getAttendees(), request, true)) {
+            mandatoryStart = eventRange.end();
+        } if (optionalStart < eventRange.end() && hasAttendees(eventList.get(i).getAttendees(), request, false)) {
+            optionalStart = eventRange.end();
+        }
+    }
+    TimeRange range = TimeRange.fromStartEnd(mandatoryStart, TimeRange.END_OF_DAY,true);
+    if (range.duration() >= request.getDuration()) {
+        mandatoryAvailableTimes.add(range);
+    }
+    TimeRange optionalRange = TimeRange.fromStartEnd(optionalStart, TimeRange.END_OF_DAY,true);
+    if (optionalRange.duration() >= request.getDuration()) {
+        optionalAvailableTimes.add(optionalRange);
+    }
+
+    return optionalAvailableTimes.isEmpty() ? mandatoryAvailableTimes : optionalAvailableTimes;
+  }
+
+    public int compare(Event a, Event b) { 
+        //sort by start
+        return a.getWhen().start() - b.getWhen().start(); 
+    }
+
+    public static boolean hasAttendees(Set eventAttendees, MeetingRequest request, boolean checkingOptional) {
+        Collection<String> attendees = request.getAttendees();
+        Collection<String> optionalAttendees = request.getOptionalAttendees();
+        // if (checkingOptional) {
+        //     attendees.addAll(optionalAttendees);
+        // }
+        for (String attendee : attendees) {
+            if (eventAttendees.contains(attendee)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void checkIfEventConflicts(Collection<TimeRange> availableTimes, Event event, MeetingRequest request, boolean optional, int start) {
+        if (hasAttendees(event.getAttendees(), request, optional)) {
+            TimeRange eventRange = event.getWhen();
 
             if (start < eventRange.start()) {
                 TimeRange range = TimeRange.fromStartEnd(start, eventRange.start(), false);
@@ -46,25 +89,4 @@ public final class FindMeetingQuery implements Comparator<Event> {
             }
         }
     }
-    TimeRange range = TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY,true);
-    if (range.duration() >= request.getDuration()) {
-        availableTimes.add(range);
-    }
-    return availableTimes;
-  }
-
-    public int compare(Event a, Event b) { 
-        //sort by start
-        return a.getWhen().start() - b.getWhen().start(); 
-    }
-
-    public static boolean hasMandatoryAttendees(Set eventAttendees, Collection<String> mandatoryAttendees) {
-        for (String mandatoryAttendee : mandatoryAttendees) {
-            if (eventAttendees.contains(mandatoryAttendee)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
