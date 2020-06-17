064f77da1d85de914cd9ae57c87c3bedc318d151
Andrew Seaman <andrew.seamanco@gmail.com>
2020-06-17 19:46:35 +0000

    Added capitalizations to the start of method descriptions

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -30,9 +30,9 @@ import java.util.stream.Stream;
  * If optional attendees cannot be fit in, then only returns the times which mandatory attendees can
  * attend
  *
- * @param events a list of events that may need to be accounted for
- * @param request the specifications for the meeting to be scheduled
- * @return a Collection of available times for requested meeting
+ * @param events A list of events that may need to be accounted for
+ * @param request The specifications for the meeting to be scheduled
+ * @return A Collection of available times for requested meeting
  */
 public final class FindMeetingQuery implements Comparator<Event> {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
@@ -67,11 +67,11 @@ public final class FindMeetingQuery implements Comparator<Event> {
   /**
    * Checks if the set of event attendees has any members from the request
    *
-   * @param eventAttendees the set of people attending the event
-   * @param request the specifications of the request used to retrieve attendees and optional
+   * @param eventAttendees The set of people attending the event
+   * @param request The specifications of the request used to retrieve attendees and optional
    *     attendees
-   * @param checkingOptional a boolean if we are checking for optional attendees
-   * @return if any of the requested attendees exist in the list for eventAttendees
+   * @param checkingOptional A boolean if we are checking for optional attendees
+   * @return If any of the requested attendees exist in the list for eventAttendees
    */
   public static boolean hasAttendees(
       Set eventAttendees, MeetingRequest request, boolean checkingOptional) {
@@ -96,15 +96,14 @@ public final class FindMeetingQuery implements Comparator<Event> {
    * Checks if the time between the event and the last event is enough for the requested meeting
    * time
    *
-   * @param availableTimes a collection of times which will work for the request to be added to
-   * @param event the  event which we will check to see if there is enough time between its start
-   *     and the last
-   * meeting's end
-   * @param request the specifications for the requested meeting
-   * @param optional a boolean which determines if we are checking optional attendees as well
-   * @param start a number which represents the end of the last meeting
+   * @param availableTimes A collection of times which will work for the request to be added to
+   * @param event The  event which we will check to see if there is enough time between its start
+   *     and the last meeting's end
+   * @param request The specifications for the requested meeting
+   * @param optional A boolean which determines if we are checking optional attendees as well
+   * @param start A number which represents the end of the last meeting
    *
-   * @return the end of this event if applicable
+   * @return The end of this event if applicable
    */
   public static int checkIfEventConflicts(Collection<TimeRange> availableTimes, Event event,
       MeetingRequest request, boolean optional, int end) {
@@ -128,9 +127,9 @@ public final class FindMeetingQuery implements Comparator<Event> {
    * Adds the time to the collection if the time between the final meeting and the end of the
    * day if there is enough time
    * 
-   * @param timeOfLastMeeting the end time of the last meeting
-   * @param request the requested meeting specifications
-   * @param availableTimes the collection of times to add to
+   * @param timeOfLastMeeting The end time of the last meeting
+   * @param request The requested meeting specifications
+   * @param availableTimes The collection of times to add to
    */
   public static void addFinalRange(int timeOfLastMeeting, MeetingRequest request, Collection<TimeRange> availableTimes) {
     TimeRange range = TimeRange.fromStartEnd(timeOfLastMeeting, TimeRange.END_OF_DAY, true);
