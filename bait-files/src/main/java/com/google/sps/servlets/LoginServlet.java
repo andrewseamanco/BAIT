b9028ba301c7b606eed6c96b96e945e3e173d66f
Andrew Seaman <andrew.seamanco@gmail.com>
2020-06-25 15:43:50 +0000

    Add template from portfolio

---
+++ bait-files/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.google.sps.servlets;
+
+// import static com.google.sps.data.Keys.ID_ENTITY_PROPERTY;
+// import static com.google.sps.data.Keys.USERNAME_ENTITY_PROPERTY;
+// import static com.google.sps.data.Keys.USER_ENTITY;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("login")
+public class LoginServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    PrintWriter out = response.getWriter();
+    response. setContentType("text/html");
+
+    if (!userService.isUserLoggedIn()) {
+      String loginUrl = userService.createLoginURL("/login");
+      out.println("<h2>Log In Or Sign Up!</h2>");
+      out.println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+    } else {
+      String logoutUrl = userService.createLogoutURL("/login");
+      out.println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
+      out.println("<p>Username:");
+      out.println("<form method=\"POST\" action=\"login\">");
+      out.println("<input name=\"username\" >");
+      out.println("<br />");
+      out.println("<button>Submit</button>");
+      out.println("</form>");
+    }
+    return;
+  }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/login");
+      return;
+    }
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity newUser = new Entity("User", userService.getCurrentUser().getUserId());
+    newUser.setProperty("Id", userService.getCurrentUser().getUserId());
+    newUser.setProperty("Username", request.getParameter("Username"));
+
+    datastore.put(newUser);
+
+    response.sendRedirect("/profile.html");
+  }
+}
