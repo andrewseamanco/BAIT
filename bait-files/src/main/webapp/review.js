0150e5c46904436af79dcb3bb7b247b5f3aeb8f9
Iman Herzi <imanherzi@gmail.com>
2020-07-06 15:41:56 +0000

    Created and styled manual review page.

---
+++ bait-files/src/main/webapp/review.js
@@ -0,0 +1,17 @@
+function getPanels() {
+  var accordian = document.getElementsByClassName('accordion');
+  var i;
+
+  for (i = 0; i < accordian.length; i++) {
+    accordian[i].addEventListener('click', function() {
+      this.classList.toggle('active');
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } 
+      else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  }
+}
