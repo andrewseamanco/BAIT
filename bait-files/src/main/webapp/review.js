551072b2aefb0761cc00bcafa24544f62b43514a
Iman Herzi <imanherzi@gmail.com>
2020-07-06 16:29:31 +0000

    Fixed formatting.

--- bait-files/src/main/webapp/review.js
+++ bait-files/src/main/webapp/review.js
@@ -1,15 +1,12 @@
 function getPanels() {
-  var accordian = document.getElementsByClassName('accordion');
-  var i;
-
-  for (i = 0; i < accordian.length; i++) {
+  const accordian = document.getElementsByClassName('accordion');
+  for (let i = 0; i < accordian.length; i++) {
     accordian[i].addEventListener('click', function() {
       this.classList.toggle('active');
       var panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
-      } 
-      else {
+      } else {
         panel.style.maxHeight = panel.scrollHeight + 'px';
       }
     });
