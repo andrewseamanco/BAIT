22b988a821bffaa9efcf6cea6517ba0d5227d199
Andrew Seaman <andrew.seamanco@gmail.com>
2020-07-07 00:17:47 +0000

    Format and alphabetize

--- bait-files/src/main/java/com/google/LoginFilter.java
+++ bait-files/src/main/java/com/google/LoginFilter.java
@@ -1,19 +1,8 @@
 package com.google;
 
-import java.io.IOException;
+import static com.google.sps.data.Keys.ID_ENTITY_PROPERTY;
+import static com.google.sps.data.Keys.USER_ENTITY;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -23,85 +12,97 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import static com.google.sps.data.Keys.USER_ENTITY;
-import static com.google.sps.data.Keys.ID_ENTITY_PROPERTY;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
 import java.net.URLEncoder;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/")
 public class LoginFilter implements Filter {
+  @Override
+  public void init(FilterConfig config) throws ServletException {
+    // If you have any <init-param> in web.xml, then you could get them
+    // here by config.getInitParameter("name") and assign it as field.
+  }
 
-    @Override
-    public void init(FilterConfig config) throws ServletException {
-        // If you have any <init-param> in web.xml, then you could get them
-        // here by config.getInitParameter("name") and assign it as field.
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) res;
+    HttpSession session = request.getSession(false);
+    UserService userService = UserServiceFactory.getUserService();
+
+    // Case: User is not logged in
+    if (!userService.isUserLoggedIn()) {
+      if (request.getRequestURI().endsWith("login")) {
+        chain.doFilter(req, res);
+        return;
+      } else {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/login.jsp");
+        requestDispatcher.forward(request, response);
+        return;
+      }
     }
-
-
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpSession session = request.getSession(false);
-        UserService userService = UserServiceFactory.getUserService();
-
-
-        //Case: User is not logged in
-        if (!userService.isUserLoggedIn()) {
-            if (request.getRequestURI().endsWith("login")) {
-                chain.doFilter(req, res);
-                return;
-            } else {
-                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/login.jsp");
-                requestDispatcher.forward(request, response);
-                return;
-            }
+    // Case: User is logged in
+    else {
+      if (!isRegistered(userService.getCurrentUser().getUserId())
+          && !request.getRequestURI().endsWith("logout")) {
+        // User is submitting form for registration
+        if (request.getRequestURI().endsWith("register")) {
+          chain.doFilter(req, res);
+          return;
         }
-        //Case: User is logged in
+        // User is not registered and is trying to access restricted material
         else {
-            if (!isRegistered(userService.getCurrentUser().getUserId()) && !request.getRequestURI().endsWith("logout")) {
-                //User is submitting form for registration
-                if (request.getRequestURI().endsWith("register")) {
-                    chain.doFilter(req, res);
-                    return;
-                }
-                //User is not registered and is trying to access restricted material
-                else {
-                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/register.jsp");
-                    requestDispatcher.forward(request, response);
-                    return;
-                }
-            }
-         }
-
-         //Is logged in and registered but is trying to access profile jsp or now restricted register page
-         if (request.getRequestURI().endsWith("profile.jsp") || request.getRequestURI().endsWith("register.jsp") || request.getRequestURI().endsWith("login.jsp")) {
-            System.out.println("here");
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/profile.jsp");
-            requestDispatcher.forward(request, response);
-            return;
-         }
-
-         //Case: User is logged in and registered and wants to access site resource
-         chain.doFilter(req, res);
-         return;
+          RequestDispatcher requestDispatcher =
+              request.getRequestDispatcher("WEB-INF/register.jsp");
+          requestDispatcher.forward(request, response);
+          return;
+        }
+      }
     }
 
-    public boolean isRegistered(String id) {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query(USER_ENTITY)
-          .setFilter(new Query.FilterPredicate(
-            ID_ENTITY_PROPERTY, Query.FilterOperator.EQUAL, id));
-        PreparedQuery results = datastore.prepare(query);
-        Entity entity = results.asSingleEntity();
-        return entity!=null;
+    // Is logged in and registered but is trying to access profile jsp or now restricted register
+    // page
+    if (request.getRequestURI().endsWith("profile.jsp")
+        || request.getRequestURI().endsWith("register.jsp")
+        || request.getRequestURI().endsWith("login.jsp")) {
+      System.out.println("here");
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/profile.jsp");
+      requestDispatcher.forward(request, response);
+      return;
     }
 
-    @Override
-    public void destroy() {
-        // If you have assigned any expensive resources as field of
-        // this Filter class, then you could clean/close them here.
-    }
+    // Case: User is logged in and registered and wants to access site resource
+    chain.doFilter(req, res);
+    return;
+  }
 
+  public boolean isRegistered(String id) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query(USER_ENTITY)
+                      .setFilter(new Query.FilterPredicate(
+                          ID_ENTITY_PROPERTY, Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    return entity != null;
+  }
+
+  @Override
+  public void destroy() {
+    // If you have assigned any expensive resources as field of
+    // this Filter class, then you could clean/close them here.
+  }
 }
--- bait-files/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ bait-files/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -11,19 +11,18 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
-
 
 @WebServlet("/register")
 public class LoginServlet extends HttpServlet {
-
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
     PrintWriter out = response.getWriter();
 
     boolean filledOutUsername = request.getParameter(USERNAME_ENTITY_PROPERTY) != null;
@@ -31,27 +30,29 @@ public class LoginServlet extends HttpServlet {
     boolean filledOutLastName = request.getParameter(LAST_NAME_ENTITY_PROPERTY) != null;
 
     if (usernameTaken(request.getParameter(USERNAME_ENTITY_PROPERTY))) {
-        response. setContentType("text/html");
-        out.println("Username is already taken.  Please try another username");
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/register.jsp");
-        requestDispatcher.forward(request, response);
-        return;
-    } else if (!filledOutLastName || !filledOutFirstName || !filledOutUsername){
-        response. setContentType("text/html");
-        out.println("Please fill out all fields before submitting");
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/register.jsp");
-        requestDispatcher.forward(request, response);
-        return;
+      response.setContentType("text/html");
+      out.println("Username is already taken.  Please try another username");
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/register.jsp");
+      requestDispatcher.forward(request, response);
+      return;
+    } else if (!filledOutLastName || !filledOutFirstName || !filledOutUsername) {
+      response.setContentType("text/html");
+      out.println("Please fill out all fields before submitting");
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/register.jsp");
+      requestDispatcher.forward(request, response);
+      return;
     } else {
-        UserService userService = UserServiceFactory.getUserService();
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    
-        Entity newUser = new Entity(USER_ENTITY, userService.getCurrentUser().getUserId());
-        newUser.setProperty(ID_ENTITY_PROPERTY, userService.getCurrentUser().getUserId());
-        newUser.setProperty(USERNAME_ENTITY_PROPERTY, request.getParameter(USERNAME_ENTITY_PROPERTY));
-        newUser.setProperty(FIRST_NAME_ENTITY_PROPERTY, request.getParameter(FIRST_NAME_ENTITY_PROPERTY));
-        newUser.setProperty(LAST_NAME_ENTITY_PROPERTY, request.getParameter(LAST_NAME_ENTITY_PROPERTY));
-        newUser.setProperty(IS_ADMIN_ENTITY_PROPERTY, false);
+      UserService userService = UserServiceFactory.getUserService();
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+      Entity newUser = new Entity(USER_ENTITY, userService.getCurrentUser().getUserId());
+      newUser.setProperty(ID_ENTITY_PROPERTY, userService.getCurrentUser().getUserId());
+      newUser.setProperty(USERNAME_ENTITY_PROPERTY, request.getParameter(USERNAME_ENTITY_PROPERTY));
+      newUser.setProperty(
+          FIRST_NAME_ENTITY_PROPERTY, request.getParameter(FIRST_NAME_ENTITY_PROPERTY));
+      newUser.setProperty(
+          LAST_NAME_ENTITY_PROPERTY, request.getParameter(LAST_NAME_ENTITY_PROPERTY));
+      newUser.setProperty(IS_ADMIN_ENTITY_PROPERTY, false);
     }
 
     RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/profile.jsp");
@@ -61,11 +62,10 @@ public class LoginServlet extends HttpServlet {
   public boolean usernameTaken(String username) {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query = new Query(USER_ENTITY)
-      .setFilter(new Query.FilterPredicate(
-        USERNAME_ENTITY_PROPERTY, Query.FilterOperator.EQUAL, username));
+                      .setFilter(new Query.FilterPredicate(
+                          USERNAME_ENTITY_PROPERTY, Query.FilterOperator.EQUAL, username));
     PreparedQuery results = datastore.prepare(query);
     Entity entity = results.asSingleEntity();
-    return entity!=null;
+    return entity != null;
   }
-  
 }
