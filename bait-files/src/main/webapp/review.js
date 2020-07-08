8d16696de9f65232d70587d559a84b620e8f00d4
Iman Herzi <imanherzi@gmail.com>
2020-07-08 01:14:07 +0000

    Made requested changes.

--- bait-files/src/main/webapp/review.js
+++ bait-files/src/main/webapp/review.js
@@ -1,13 +1,13 @@
 function getPanels() {
-  const acc = document.getElementsByClassName('accordion');
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function() {
+  const accordion = document.getElementsByClassName('accordion');
+  for (fold of accordion) {
+    fold.addEventListener('click', function() {
       this.classList.toggle('active');
       const panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
+      if (panel.style.maxHeight) {          //when accordion button is clicked, if panel is visible closes panel by setting maxHeight to null
         panel.style.maxHeight = null;
       } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
+        panel.style.maxHeight = panel.scrollHeight + 'px';  //when accordion button is clicked, if panel is not visible, set maxHeight so panel is displayed
       }
     });
   }
