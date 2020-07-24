24952d685a901b9d98f76f05f72f205b5c035849
Andrew Seaman <andrew.seamanco@gmail.com>
2020-07-24 19:09:15 +0000

    Add country for address form

---
+++ bait-files/src/main/webapp/submission.js
@@ -0,0 +1,17 @@
+function loadAmericanAddress() {
+    //Address
+    let addressLine = document.createElement("input");
+    addressLine.type = "text";
+    
+    <input type="phone" id="phone-input" name="phone-input"></input>
+    //State
+    //City
+    //Postal Code
+}
+
+function loadCanadianAddress() {
+    //Address line
+    //Province
+    //City
+    //Postal Code
+}
