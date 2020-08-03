2aeeb967c686109d1fd156191ac2a237c6e1918f
becca <sunbecca@gmail.com>
2020-08-03 19:29:50 +0000

    modified helper functions

--- bait-files/src/main/webapp/submission.js
+++ bait-files/src/main/webapp/submission.js
@@ -1,5 +1,5 @@
 /** Do not allow spaces in username input */
-function keyIsValidForUsername(event) {
+function checkSpace(event) {
   if (event.key === ' ') {
     event.preventDefault();
     return false;
@@ -12,7 +12,13 @@ function keyIsValidForName(event) {
   return /[a-z, ]/i.test(event.key);
 }
 
-/** Only allow numbers or hypens for phone number input */
+/** Only allow numbers for phone number input */
 function keyIsValidForPhoneNumber(event) {
-  return /[a-z, ]/i.test(event.key);
+  var charCode = (event.which) ? event.which : event.keyCode;
+  if (charCode != 46 && charCode != 45 && charCode > 31 &&
+      (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+
+  return true;
 }
